0d85ff46f8c1be6a244bd16694478381f21347cd
Brennan Whitfield <bwhitfield@wsutech.edu>
2023-03-09 09:33:22 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class AreaOfRectangle
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the Length of a Rectangle: ");
+        int Length = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the Width of a Rectangle: ");
+        int Width = Convert.ToInt32(Console.ReadLine());
+
+        int Area = Length * Width;
+        Console.WriteLine($"Area of Length {Length} and Width {Width} Rectangle is {Area}");
+        Console.ReadKey();
+    }
+}
